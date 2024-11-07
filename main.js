3e16b7240856952dce9a21fde48893332f7ef981
rod-r-igo <163418849+rod-r-igo@users.noreply.github.com>
2024-11-07 09:53:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+let numero1 = parseInt(prompt("Informe o número 1: "));
+let numero2 = parseInt(prompt("Informe o número 2: "));
+let soma = numero1 + numero2;
+document.write("A soma é igual a:", soma);
